28f239ab78c8bebe05da06e9621b9dd7d64a67c5
jonjohnsonjr <jonjohnson@google.com>
2019-12-09 11:07:23 -0800

    Update ggcr (#113)
    Mostly to pick up media type mutation, but this includes a bunch of
    other fixes.
    
    https://github.com/google/go-containerregistry/compare/ff1ac7f97758299e13f746209d8c0f199923d89a...73724ba06bda116382dafb17b39b682ffab4dc36

--- vendor/github.com/google/go-containerregistry/pkg/v1/remote/index.go
+++ vendor/github.com/google/go-containerregistry/pkg/v1/remote/index.go
@@ -162,16 +162,9 @@ func (r *remoteIndex) childByHash(h v1.Hash) (*Descriptor, error) {
 	return nil, fmt.Errorf("no child with digest %s in index %s", h, r.Ref)
 }
 
-func (r *remoteIndex) childRef(h v1.Hash) (name.Reference, error) {
-	return name.ParseReference(fmt.Sprintf("%s@%s", r.Ref.Context(), h), name.StrictValidation)
-}
-
 // Convert one of this index's child's v1.Descriptor into a remote.Descriptor, with the given platform option.
 func (r *remoteIndex) childDescriptor(child v1.Descriptor, platform v1.Platform) (*Descriptor, error) {
-	ref, err := r.childRef(child.Digest)
-	if err != nil {
-		return nil, err
-	}
+	ref := r.Ref.Context().Digest(child.Digest.String())
 	manifest, desc, err := r.fetchManifest(ref, []types.MediaType{child.MediaType})
 	if err != nil {
 		return nil, err
