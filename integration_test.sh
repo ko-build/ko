e638e04142bf49145e4a6cede3e078fc3c60934d
Jon Donovan <jondonovan@google.com>
2020-07-26 22:04:54 -0700

    Move integration test back and actually test the outputs.
    I realize now this is run in a travis CI job :) So I'll make it actually work.

---
+++ integration_test.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+ROOT_DIR=$(dirname $0)
+
+pushd "$ROOT_DIR"
+
+ROOT_DIR="$(pwd)"
+
+echo "Running smoke test."
+go install ./cmd/ko
+ko apply -f ./cmd/ko/test -L
+
+echo "Moving GOPATH into /tmp/ to test modules behavior."
+export ORIGINAL_GOPATH="$GOPATH"
+export GOPATH="$(mktemp -d)"
+
+pushd "$GOPATH" || exit 1
+
+echo "Copying ko to temp gopath."
+mkdir -p "$GOPATH/src/github.com/google/ko"
+cp -r "$ROOT_DIR/"* "$GOPATH/src/github.com/google/ko/"
+
+echo "Downloading github.com/go-training/helloworld"
+go get -d github.com/go-training/helloworld
+
+pushd "$GOPATH/src/github.com/google/ko" || exit 1
+
+echo "Replacing hello world in vendor with TEST."
+sed -i 's/Hello World/TEST/g' ./vendor/github.com/go-training/helloworld/main.go
+
+echo "Building ko"
+
+RESULT="$(GO111MODULE="on" GOFLAGS="-mod=vendor" go build ./cmd/ko)"
+
+echo "Beginning scenarios."
+
+FILTER="[^ ].*@sha256[^ ]*"
+
+echo "1. GOPATH mode should always create an image that outputs 'Hello World'"
+RESULT="$(GO111MODULE=off ./ko publish github.com/go-training/helloworld 2>1 | grep "$FILTER" | xargs -I% docker run %)"
+if [[ "$RESULT" != *"Hello World"** ]]; then
+  echo "Test FAILED. Saw $RESULT" && exit 1
+else
+  echo "test PASSED"
+fi
+
+echo "2. Go module auto mode should create an image that outputs 'Hello World' when run outside the module."
+
+pushd .. || exit 1
+RESULT="$(GO111MODULE=auto GOFLAGS="-mod=vendor" ./ko/ko publish github.com/go-training/helloworld 2>1 | grep "$FILTER" | xargs -I% docker run %)"
+if [[ "$RESULT" != *"Hello World"* ]]; then
+  echo "Test FAILED. Saw $RESULT" && exit 1
+else
+  echo "Test PASSED"
+fi
+
+popd || exit 1
+
+echo "3. Auto inside the module with vendoring should output TEST"
+
+RESULT="$(GO111MODULE=auto GOFLAGS="-mod=vendor" ./ko publish github.com/go-training/helloworld 2>1 | grep "$FILTER" | xargs -I% docker run %)"
+if [[ "$RESULT" != *"TEST"* ]]; then
+  echo "Test FAILED. Saw $RESULT" && exit 1
+else
+  echo "Test PASSED"
+fi
+
+echo "4. Auto inside the module without vendoring should output Hello World"
+RESULT="$(GO111MODULE=auto GOFLAGS="" ./ko publish github.com/go-training/helloworld 2>1 | grep "$FILTER" | xargs -I% docker run %)"
+if [[ "$RESULT" != *"Hello World"* ]]; then
+  echo "Test FAILED. Saw $RESULT" && exit 1
+else
+  echo "Test PASSED"
+fi
+
+echo "5. On inside the module with vendor should output TEST."
+RESULT="$(GO111MODULE=on GOFLAGS="-mod=vendor" ./ko publish github.com/go-training/helloworld 2>1 | grep "$FILTER" | xargs -I% docker run %)"
+if [[ "$RESULT" != *"TEST"* ]]; then
+  echo "Test FAILED. Saw $RESULT" && exit 1
+else
+  echo "Test PASSED"
+fi
+
+echo "6. On inside the module without vendor should output Hello World"
+RESULT="$(GO111MODULE=on GOFLAGS="" ./ko publish github.com/go-training/helloworld 2>1 | grep "$FILTER" | xargs -I% docker run %)"
+if [[ "$RESULT" != *"Hello World"* ]]; then
+  echo "Test FAILED. Saw $RESULT" && exit 1
+else
+  echo "Test PASSED"
+fi
+
+echo "7. On outside the module should fail."
+pushd .. || exit 1
+GO111MODULE=on ./ko/ko publish github.com/go-training/helloworld && exit 1
+
+popd || exit 1
+popd || exit 1
+popd || exit 1
