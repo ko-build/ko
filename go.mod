2cff5109597e434e455ed517064703c235362d58
Markus Thömmes <markusthoemmes@me.com>
2020-09-04 17:49:17 +0200

    Bump Kubernetes dependencies to 1.18.

--- go.mod
+++ go.mod
@@ -20,13 +20,13 @@ require (
 	golang.org/x/tools v0.0.0-20200210192313-1ace956b0e17
 	gopkg.in/yaml.v3 v3.0.0-20200121175148-a6ecf24a6d71
 	gotest.tools/v3 v3.0.2 // indirect
-	k8s.io/apimachinery v0.18.2
+	k8s.io/apimachinery v0.18.8
 	k8s.io/cli-runtime v0.17.1
 	sigs.k8s.io/kind v0.8.1
 )
 
 replace (
-	k8s.io/apimachinery => k8s.io/apimachinery v0.17.1
-	k8s.io/cli-runtime => k8s.io/cli-runtime v0.17.1
-	k8s.io/client-go => k8s.io/client-go v0.17.1
+	k8s.io/apimachinery => k8s.io/apimachinery v0.18.8
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.18.8
+	k8s.io/client-go => k8s.io/client-go v0.18.8
 )
--- vendor/k8s.io/apimachinery/pkg/util/wait/doc.go
+++ vendor/k8s.io/apimachinery/pkg/util/wait/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 The Kubernetes Authors.
+Copyright 2014 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,10 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +k8s:deepcopy-gen=package
-// +k8s:openapi-gen=true
-// +k8s:defaulter-gen=TypeMeta
-
-// +groupName=meta.k8s.io
-
-package v1beta1 // import "k8s.io/apimachinery/pkg/apis/meta/v1beta1"
+// Package wait provides tools for polling or listening for changes
+// to a condition.
+package wait // import "k8s.io/apimachinery/pkg/util/wait"
