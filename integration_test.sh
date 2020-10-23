d79b7d1a9ab436fa02cd6eb89f7ba5ad237c02e0
Matt Moore <mattmoor@vmware.com>
2020-10-23 08:32:21 -0700

    Move the kind and integration tests to actions

--- integration_test.sh
+++ integration_test.sh
@@ -9,12 +9,6 @@ pushd "$ROOT_DIR"
 
 ROOT_DIR="$(pwd)"
 
-echo "Running smoke test."
-go install ./cmd/ko
-# Travis runs this against a kind cluster so properly use the kind publisher.
-KO_DOCKER_REPO=kind.local ko apply --platform=all -f ./cmd/ko/test
-kubectl wait --timeout=10s --for=condition=Ready pod/kodata
-
 echo "Moving GOPATH into /tmp/ to test modules behavior."
 export ORIGINAL_GOPATH="$GOPATH"
 export GOPATH="$(mktemp -d)"
