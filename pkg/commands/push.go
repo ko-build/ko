02dcf04e74ff1678843bed7eebf0406c75cfed42
Arve Knudsen <arve.knudsen@gmail.com>
2022-02-08 15:07:44 +0100

    Make push sub-command take an OCI layout
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/commands/push.go
+++ pkg/commands/push.go
@@ -20,23 +20,32 @@ import (
 	"io"
 	"os"
 
+	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/empty"
+	"github.com/google/go-containerregistry/pkg/v1/layout"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
 	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/google/go-containerregistry/pkg/v1/types"
+	"github.com/google/ko/pkg/commands/options"
+	"github.com/google/ko/pkg/publish"
+	ocimutate "github.com/sigstore/cosign/pkg/oci/mutate"
+	"github.com/sigstore/cosign/pkg/oci/signed"
 	"github.com/spf13/cobra"
 )
 
 // addPush augments our CLI surface with push.
 func addPush(topLevel *cobra.Command) {
 	push := &cobra.Command{
-		Use:   "push TARBALL...",
-		Short: "Push manifest list from the given tarballs.",
-		Long:  `This sub-command loads container images from the given tarballs, and pushes a manifest list combining them to the configured registry.`,
+		Use:   "push APP OCI-LAYOUT",
+		Short: "Push manifest list from the given OCI image layout.",
+		Long:  `This sub-command loads a manifest list from the given OCI image layout, and pushes it to the configured repository.`,
 		Example: `
-  # Push manifest list from images in tarballs to a Docker Registry as:
-  #   ${KO_DOCKER_REPO}/<package name>-<hash of import path>
-  ko push images.tar.gz`,
-		Args: cobra.MinimumNArgs(1),
+  # Push manifest list from OCI image layout to a Docker Registry as:
+  #   ${KO_DOCKER_REPO}/<app>
+  ko push example docker-images`,
+		Args: cobra.MinimumNArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := cmd.Context()
 
@@ -51,32 +60,79 @@ func addPush(topLevel *cobra.Command) {
 }
 
 func pushManifestList(ctx context.Context, args []string) error {
-	var images []v1.Image
-	for _, fpath := range args {
-		is, err := loadImages(fpath)
+	idx, err := layout.ImageIndexFromPath(args[1])
+	if err != nil {
+		return err
+	}
+	mt, err := idx.MediaType()
+	if err != nil {
+		return err
+	}
+	im, err := idx.IndexManifest()
+	if err != nil {
+		return err
+	}
+
+	var adds []ocimutate.IndexAddendum
+
+	fmt.Printf("The image index has media type %s and %d image manifests\n", mt, len(im.Manifests))
+	var nestedMt types.MediaType
+	for _, d := range im.Manifests {
+		fmt.Printf("Manifest %s with type %s\n", d.Digest.Hex, d.MediaType)
+		nestedIdx, err := idx.ImageIndex(d.Digest)
 		if err != nil {
 			return err
 		}
 
-		for _, img := range is {
-			h, err := img.ConfigName()
-			if err != nil {
-				return err
-			}
-			d, err := img.Digest()
-			if err != nil {
-				return err
-			}
-			fmt.Printf("Loaded image %s, digest %s, from %q\n", h.Hex, d.Hex, fpath)
+		nestedMt, err = nestedIdx.MediaType()
+		if err != nil {
+			return err
 		}
-		images = append(images, is...)
+		mm, err := nestedIdx.IndexManifest()
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Nested index with type %s and %d manifests\n", nestedMt, len(mm.Manifests))
+		for _, m := range mm.Manifests {
+			img, err := nestedIdx.Image(m.Digest)
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Manifest with type %s, digest %s and platform %#v\n", m.MediaType, m.Digest.Hex, m.Platform)
+			fmt.Printf("Adding addendum - URLs: %#v, media type: %s, annotations: %#v, platform: %#v\n", m.URLs, m.MediaType, m.Annotations, m.Platform)
+			adds = append(adds, ocimutate.IndexAddendum{
+				Add: signed.Image(img),
+				Descriptor: v1.Descriptor{
+					URLs:        m.URLs,
+					MediaType:   m.MediaType,
+					Annotations: m.Annotations,
+					Platform:    m.Platform,
+				},
+			})
+		}
 	}
 
-	fmt.Printf("Loaded %d image(s)\n", len(images))
+	newIdx := ocimutate.AppendManifests(mutate.IndexMediaType(empty.Index, nestedMt), adds...)
+	fmt.Printf("Created index %#v\n", newIdx)
 
-	// TODO: Make manifest list
+	dockerRepo := os.Getenv("KO_DOCKER_REPO")
+	if dockerRepo == "" {
+		return fmt.Errorf("KO_DOCKER_REPO environment variable is unset")
+	}
 
-	// TODO: Push manifest list to registry
+	// TODO
+	namer := options.MakeNamer(&options.PublishOptions{})
+	publisher, err := publish.NewDefault(dockerRepo,
+		publish.WithUserAgent(ua()),
+		publish.WithAuthFromKeychain(authn.DefaultKeychain),
+		publish.WithNamer(namer),
+	)
+	if err != nil {
+		return err
+	}
+	publisher.Publish(ctx, newIdx, args[0])
 
 	return nil
 }
@@ -117,6 +173,17 @@ func loadImages(fpath string) ([]v1.Image, error) {
 		}
 
 		imgs = append(imgs, img)
+
+		m, err := tarball.LoadManifest(func() (io.ReadCloser, error) {
+			return os.Open(fpath)
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to load manifest for %q from %q: %w", t, fpath, err)
+		}
+
+		for _, d := range m {
+			fmt.Printf("Descriptor config: %s\n", d.Config)
+		}
 	}
 
 	return imgs, nil
