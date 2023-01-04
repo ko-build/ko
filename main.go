5892f4113f391d93f1d22ada02f504daad8a7c78
Jason Hall <jason@chainguard.dev>
2023-01-04 11:30:24 -0500

    remove repeated error message on failure

--- main.go
+++ main.go
@@ -18,7 +18,6 @@ package main
 
 import (
 	"context"
-	"log"
 	"os"
 	"os/signal"
 
@@ -29,6 +28,6 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 	if err := commands.Root.ExecuteContext(ctx); err != nil {
-		log.Fatal("error during command execution:", err)
+		os.Exit(1)
 	}
 }
