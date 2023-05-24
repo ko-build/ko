64e11c35b6c4565a77b11dffc3e9e3778f59698f
cpanato <ctadeu@gmail.com>
2023-05-24 15:33:58 +0200

    update to the new format
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2021 KO Build Authors All Rights Reserved.
+# Copyright 2021 ko Build Authors All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
