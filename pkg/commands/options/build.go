fbc9f3aab8b81f58d6c9810e9bbe3f7ea6b0743a
cpanato <ctadeu@gmail.com>
2023-05-24 15:19:51 +0200

    update boilerplate file to be KO Build Authors
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- pkg/commands/options/build.go
+++ pkg/commands/options/build.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Google LLC All Rights Reserved.
+Copyright 2019 KO Build Authors All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
